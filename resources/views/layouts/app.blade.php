1fdbe817af5418d44c46061505a9358eba1379c2
Marcos <marcos.ferrini@gmail.com>
2019-07-04 21:52:37 -0300

    login, registro, perfil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,50 +1,50 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+		<!-- CSRF Token -->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+		<title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+		<!-- Scripts -->
+		<script src="{{ asset('js/app.js') }}" defer></script>
+
+		<!-- Fonts -->
+		<link rel="dns-prefetch" href="//fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Cinzel|Satisfy" rel="stylesheet">
-    
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/master.css') }}" rel="stylesheet">
-    
-    <title>Phi-Organic</title>
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Cinzel|Satisfy" rel="stylesheet">
+
+		<!-- Styles -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+		<link href="{{ asset('css/master.css') }}" rel="stylesheet">
+
+		<title>Phi-Organic</title>
 
 
-</head>
-<body>
-    <div id="app">
+	</head>
+	<body>
+		<div id="app">
 
-        @include('partials/header')
+			@include('partials/header')
 
-        <main class="py-4">
-        	
-        	@includeWhen(!Route::is('login') &&  !Route::is('register'), 'partials/navbar')
-        	
-        	
-          @yield('content')
-          
-        </main>
-    </div>
-    @include('partials/footer')
+			<main class="py-4">
 
-    @include('partials.scripts')
-</body>
+				@includeWhen(!Route::is('login') &&  !Route::is('register'), 'partials/navbar')
+
+
+				@yield('content')
+
+			</main>
+		</div>
+		@include('partials/footer')
+
+		@include('partials.scripts')
+	</body>
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,7 +9,7 @@
 				<!-- Left Side Of Navbar -->
 				<ul class="navbar-nav mr-auto">
 					<a class="navbar-brand" href="{{ url('/') }}">
-					<!--{{ config('app.name', 'Laravel') }}-->
+						<!--{{ config('app.name', 'Laravel') }}-->
 						<a class="logo" href="index.php"><img src="<?PHP echo Storage::url('img/logo_hoja.png') ?>" width="150" alt="logo" ></a>
 					</a>
 				</ul>
@@ -18,34 +18,34 @@
 				<ul class="navbar-nav ml-auto">
 					<!-- Authentication Links -->
 					@guest
-						<li class="nav-item">
-							<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-						</li>
-						@if (Route::has('register'))
-							<li class="nav-item">
-								<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-							</li>
-						@endif
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+					</li>
+					@if (Route::has('register'))
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+					</li>
+					@endif
 					@else
-						<li class="nav-item dropdown">
-							<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-								Hola {{ Auth::user()->name }} <span class="caret"></span>
-								<img height="35" width="35" class="img-fluid rounded-circle" src="{{ asset('storage/avatars/' . auth()->user()->avatar) }}">
+					<li class="nav-item dropdown">
+						<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+							Hola {{ Auth::user()->name }} <span class="caret"></span>
+							<img height="35" width="35" class="img-fluid rounded-circle" src="{{ asset('storage/avatars/' . auth()->user()->avatar) }}">
+						</a>
+
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="/perfil"><i class="navuser">{{ __('Editar Perfil') }}</i></a>
+							<a class="dropdown-item" href="{{ route('logout') }}"
+								onclick="event.preventDefault();
+								document.getElementById('logout-form').submit();">
+								{{ __('Logout') }}
 							</a>
 
-							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-								<a class="dropdown-item" href="/perfil"><i class="navuser">{{ __('Editar Perfil') }}</i></a>
-								<a class="dropdown-item" href="{{ route('logout') }}"
-									 onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-									{{ __('Logout') }}
-								</a>
-
-								<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-									@csrf
-								</form>
-							</div>
-						</li>
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+								@csrf
+							</form>
+						</div>
+					</li>
 					@endguest
 					<li class="nav-item">
 						<a class="nav-link" href="#">
@@ -56,3 +56,4 @@
 			</div>
 		</div>
 	</nav>
+</header>
